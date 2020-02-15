2d132555706f241a7438f91bed2c08d73676796a
LaksCastro <lakhsmycastro336@gmail.com>
2020-02-14 21:41:36 -0400

    moving local scripts to end

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,2 @@
-window.onload = function(){
-    const footer = document.getElementsByClassName("main-footer")[0];
-    footer.textContent = footer.textContent + new Date().getFullYear();
-}
+const footer = document.getElementsByClassName("main-footer")[0];
+footer.textContent = footer.textContent + new Date().getFullYear();
