d74cbc8a30c8224730ebfdec61b844fd3f405850
LaksCastro <lakhsmycastro336@gmail.com>
2020-02-15 00:07:35 -0400

    adding ui card with random background colors

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,14 @@
 const footer = document.getElementsByClassName("main-footer")[0];
 footer.textContent = footer.textContent + new Date().getFullYear();
+
+const stepsWrappers = document.getElementsByClassName("step-wrapper");
+for (let step of stepsWrappers) {
+    step.style.backgroundColor = `rgb(${randomIntFromInterval(
+        130,
+        200
+    )}, ${randomIntFromInterval(20, 75)}, ${randomIntFromInterval(40, 130)})`;
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
