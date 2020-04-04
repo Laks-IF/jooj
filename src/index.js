79f8ede9f421b66609477194f680961c21c26107
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-04 19:11:15 -0300

    fix express app order

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
 const cors = require("cors");
 
 const PORT = process.env.PORT || 8453;
+
+const app = express();
 
 require("./database");
 
