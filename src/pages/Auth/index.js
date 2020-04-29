00c25389f70b0a3504bf26397be1fdf04f490d17
LaksCastro <lakhsmycastro336@gmail.com>
2020-04-29 19:17:08 -0300

    make login page responsive

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import Ripple from "../../components/Ripple";
 
+import Logo from "../../assets/browser-logo-512.png";
+
 import { FaTwitter, FaGoogle, FaGithub, FaMicrosoft } from "react-icons/fa";
 
 import firebase_service from "../../services/firebase";
@@ -35,10 +37,13 @@ const methods = {
 
 export default () => (
   <S.AuthWrapper>
-    <S.AuthTitle>Faça login para continuar...</S.AuthTitle>
+    <S.AuthLogo src={Logo} alt="Flip Logotipo, imagem do aplicativo" />
+    <S.AuthTitle>
+      Mostre sua carta... hã, digo, faça login para continuar...
+    </S.AuthTitle>
     <S.AuthDescription>
-      Suas informações são usadas para identificação, não temos permissão de
-      realizar nada em seu nome
+      Suas informações são utilizadas para e somente identificação, não podemos
+      divulgar nada em seu nome, não, também não posso te enviar pack do pé
     </S.AuthDescription>
     <S.AuthButtonsWrapper>
       {Object.keys(methods).map((key) => {
@@ -53,7 +58,9 @@ export default () => (
             <S.AuthIconWrapper>
               <Icon />
             </S.AuthIconWrapper>
-            Login com {firstLetterUpper(key)}{" "}
+            <S.AuthButtonText>
+              Login com {firstLetterUpper(key)}
+            </S.AuthButtonText>
             <Ripple color="var(--ripple)" duration={1000} />
           </S.AuthButton>
         );
--- src/pages/Auth/styles.js
+++ src/pages/Auth/styles.js
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const AuthWrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: max(656px, 100vh);
 
   background: var(--background);
 
@@ -12,11 +12,19 @@ export const AuthWrapper = styled.div`
   align-items: center;
 
   padding: 1rem;
+
+  @media screen and (max-width: 379px) {
+    height: max(527px, 100vh);
+  }
 `;
 
 export const AuthButtonsWrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  @media screen and (max-width: 379px) {
+    flex-flow: row wrap;
+  }
 `;
 export const AuthButton = styled.button`
   flex: 1;
@@ -35,20 +43,55 @@ export const AuthButton = styled.button`
   position: relative;
 
   color: #fff;
+
+  transition: var(--transition);
+
+  &:hover {
+    filter: brightness(80%);
+  }
+
+  @media screen and (max-width: 379px) {
+    padding: 14px;
+  }
+`;
+export const AuthButtonText = styled.span`
+  @media screen and (max-width: 379px) {
+    display: none;
+  }
 `;
 
 export const AuthIconWrapper = styled.div`
   color: #fff;
 
-  margin-right: 12px;
+  margin-right: 14px;
 
   font-weight: bolder;
 
   display: flex;
   align-items: center;
+
+  font-size: 18px;
+
+  @media screen and (max-width: 379px) {
+    margin-right: 0px;
+  }
 `;
 
+export const AuthLogo = styled.img`
+  height: 80px;
+  animation: logo 2.5s ease-in-out 0s infinite;
+  @keyframes logo {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 export const AuthTitle = styled.h1`
+  margin-top: 2rem;
+  max-width: 500px;
   font-size: var(--font-big);
   color: var(--color-light);
   text-align: center;
