0ae7a5d8840f2a46e27cabc9530b6d7fc92aee50
LaksCastro <lakhsmycastro336@gmail.com>
2020-06-16 15:09:51 -0300

    fix: add enterTeam to default store

--- src/store/hooks/useStore.js
+++ src/store/hooks/useStore.js
@@ -1,9 +1,11 @@
 import { useContext, createContext } from "react";
+
 import { authDefault } from "../reducers/auth";
 import { loaderDefault } from "../reducers/loader";
+import { enterTeamDefault } from "../reducers/enterTeam";
 
 export const defaultStore = {
-  store: { ...authDefault, ...loaderDefault },
+  store: { ...authDefault, ...loaderDefault, ...enterTeamDefault  },
   dispatch: () => {},
 };
 
